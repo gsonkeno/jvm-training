23dfae9fe502d5142a5752ba2cdda7286f107be8
gaosong <805940564@qq.com>
2018-10-28 23:48:12 +0800

    commit gc log

--- src/main/java/com/gsonkeno/jvmtraining/jconsole/Jconsole.java
+++ src/main/java/com/gsonkeno/jvmtraining/jconsole/Jconsole.java
@@ -1,10 +1,14 @@
 package com.gsonkeno.jvmtraining.jconsole;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
  * 设置虚拟机参数为 -Xms100m -Xmx100m -XX:+UseSerialGC
+ * -verbose:gc -XX:+PrintGCDetails -XX:+PrintHeapAtGC -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -Xloggc:gc.log
+ * 默认情况下 新生代:老年代=1:2
+ * 这段代码的意思是以64KB/1000ms的速度往java堆中填充数据，一共填充1000次
  */
 public class Jconsole {
     /**
@@ -17,14 +21,20 @@ public class Jconsole {
     public static void fillHeap(int nums) throws InterruptedException {
         List<OOMObject> list = new ArrayList<>();
         for (int i = 0; i < nums; i++) {
-            Thread.sleep(100);
+            Thread.sleep(50);
             list.add(new OOMObject());
+            System.out.println(new Date());
         }
         System.gc();
-        Thread.sleep(1*1000);
+        System.out.println("invoked System.gc");
+        Thread.sleep(10*1000);
+        System.out.println(new Date());
     }
 
     public static void main(String[] args) throws InterruptedException {
         fillHeap(1000);
+        System.out.println("jumped out fillHeap");
+        Thread.sleep(10*1000);
+        System.out.println(new Date());
     }
 }
