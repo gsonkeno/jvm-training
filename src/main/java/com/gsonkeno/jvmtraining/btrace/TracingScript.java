a7ed2f1ffdd1c5d22a0cf7a8f7187ebe16c57720
gaosong <805940564@qq.com>
2018-11-03 20:16:23 +0800

    commit gc log

---
+++ src/main/java/com/gsonkeno/jvmtraining/btrace/TracingScript.java
@@ -0,0 +1,39 @@
+package com.gsonkeno.jvmtraining.btrace;
+
+import com.sun.btrace.annotations.*;
+import static com.sun.btrace.BTraceUtils.*;
+
+/**
+ * 调试脚本
+ */
+@BTrace
+public class TracingScript {
+    @OnMethod(
+            clazz = "com.gsonkeno.jvmtraining.btrace.BTraceTest",
+            method = "add",
+            location = @Location(Kind.RETURN)
+    )
+
+    public static void func(@Self com.gsonkeno.jvmtraining.btrace.BTraceTest instance,
+                            int a, int b, @Return int result){
+        println("调用堆栈:");
+        jstack();
+        println(strcat("方法参数A:", str(a)));
+        println(strcat("方法参数B:", str(b)));
+        println(strcat("方法结果:", str(result)));
+    }
+
+//    最终在jvisualvm工具面板中打印出来的内容，类似如下:
+//    调用堆栈:
+//            com.gsonkeno.jvmtraining.btrace.BTraceTest.add(BTraceTest.java:9)
+//            com.gsonkeno.jvmtraining.btrace.BTraceTest.main(Unknown Source)
+//    方法参数A:754
+//    方法参数B:584
+//    方法结果:1338
+//    调用堆栈:
+//            com.gsonkeno.jvmtraining.btrace.BTraceTest.add(BTraceTest.java:9)
+//            com.gsonkeno.jvmtraining.btrace.BTraceTest.main(Unknown Source)
+//    方法参数A:106
+//    方法参数B:309
+//    方法结果:415
+}
---
+++ src/main/java/com/gsonkeno/jvmtraining/jconsole/JvisualVMController.java
@@ -0,0 +1,24 @@
+package com.gsonkeno.jvmtraining.jconsole;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 测试jvisualVM的profiler功能
+ * 分析cpu或内存，但是在“校准”时一直超时，未能真正测试
+ */
+@RestController
+public class JvisualVMController {
+
+    @RequestMapping(value = "/jVisualVM")
+    public Map jvisualVm(){
+        Map map = new HashMap();
+        map.put("name","gs");
+        map.put("age","25");
+
+        return map;
+    }
+}
